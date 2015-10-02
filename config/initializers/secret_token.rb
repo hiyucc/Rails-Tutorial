67f8e5c02c0f44f175a72d3d58af6a77d64edf65
hideyuki-oka <hideyuki.oka@nijibox.co.jp>
2015-10-02 18:03:10 +0900

    first commit ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SfRailsTutorial::Application.config.secret_key_base = 'f4dcfbd76f80438bcee35041f31bfdf1e095fc9ddde07d92baa5a7318637f3a7ddbaa2350f97b072d79c1a2b5ebd4898da16cb3d76025eb31e360a8630a9f647'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SfRailsTutorial::Application.config.session_store :cookie_store, key: '_sf_Rails-Tutorial_session'
